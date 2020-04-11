9e8e60b3b61d566b64f1bcf6aad865c0c0e730e3
S.A.V.I.O.U.R HP15 <lead.developer@simillustra.com>
2020-04-11 15:03:21 +0100

    sizable fix

--- src/covid-routine.js
+++ src/covid-routine.js
@@ -8,19 +8,6 @@ import {
   PERCENTAGE_CASES_NEEDS_FOR_VENTILATION
 } from './constants';
 
-// let sampleCaseData = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
 let sampleCaseData = {};
 let estimatesdDataStored = {};
 
